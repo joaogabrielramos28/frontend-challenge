f037b46d081e28d7c49040b52c81b995ac8228d3
João Gabriel Fonseca <joao.fonseca@dt3.com.br>
2022-01-21 15:17:42 -0300

    Responsividade searchSection

--- my-app/src/components/searchSection/styles.ts
+++ my-app/src/components/searchSection/styles.ts
@@ -1,89 +1,131 @@
-import styled from 'styled-components'
-
+import styled from "styled-components";
 
 export const Container = styled.section`
-    width: 100%;
-    background-color:var(--green-500);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height:250px;
+  width: 100%;
+  background-color: var(--green-500);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 
-    form{
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+  form {
+    margin-top: 2rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding:20px 0;
+
+    
+  }
+
+  button {
+    width: 200px;
+    background-color: var(--green-700);
+    color: var(--white);
+    border-radius: 7px;
+    border: none;
+    padding: 14px 16px;
+    margin-top: 26px;
+    margin-left: 20px;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.7;
+      transition: opacity 0.7s;
+    }
+  }
+
+  @media (max-width: 1000px) {
+      form{
+        flex-direction: column;
+      }
+      button{
+          width:300px;
+          margin-left:0;
+      }
     }
 
-    button{
-        width:200px;
-        background-color: var(--green-700);
-        color: var(--white);
-        border-radius:7px;
-        border:none;
-        padding:14px 16px;
-        margin-top:26px;
-        margin-left:20px;
-        cursor:pointer;
+    @media(max-width: 320px) {
 
-        &:hover{
-            opacity:.7;
-            transition:opacity 0.7s;
+        button{
+            width:250px;
+            margin-left:0;
         }
     }
 
-`
-
-export const Content = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-`
-
+  
+`;
 
 export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
 
 
-    label{
-        font-size:1rem;
-        color:var(--white);
-        margin:0 20px;
-        margin-bottom:7px;
+  label {
+    font-size: 1rem;
+    color: var(--white);
+    margin: 0 20px;
+    margin-bottom: 7px;
+  }
+
+  select {
+    width: 250px;
+    padding: 12px 22px;
+    outline: none;
+    border-radius: 7px;
+  }
+
+  input {
+    width: 300px;
+    padding: 12px 22px;
+    border-radius: 7px;
+    outline: none;
+    border: none;
+
+
+    &:focus {
+      border: 2px solid var(--green-700);
+    }
+  }
+
+  #meta {
+    width: 150px;
+  }
+
+  select,
+  input {
+    margin: 0 20px;
+  }
+
+
+  @media (max-width: 1000px) {
+    select {
+      width: 350px;
     }
 
-
-    select{
-        width:250px;
-        padding:12px 22px;
-        outline:none;
-        border-radius:7px;
-    }
-
-    input{
+    input,
+    #meta {
         width:300px;
-        padding:12px 22px;
-        border-radius:7px;
-        outline:none;
-        border:none;
-
-        &:focus{
-            border:2px solid var(--green-700);
-        }
-    }
-
-    #meta{
-        width:150px;
     }
 
     select,input{
-        margin: 0 20px;
+        margin: 10px 0;
     }
 
-`
+  
+  }
+
+  @media (max-width:320px){
+    select {
+      width: 300px;
+    }
+
+    input,
+    #meta {
+        width:250px;
+    }
+  }
+`;
